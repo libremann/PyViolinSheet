e02e01017ca18ee7fe2a5f08941bb20d6498b212
Libreman <libreman@parabyte.com>
2025-07-28 19:42:41 +0330

    The main body was written. All issues related to variable names were resolved because the update_image function was taken from another personal project.

--- src/main.py
+++ src/main.py
@@ -19,6 +19,10 @@ import random
 import os
 import sys
 
+image_folder = "./Images"
+
+image_names = [f for f in os.listdir(image_folder) if f.lower().endswith(".png")] # Create a list from the image files name
+
 
 def errorfunc ():
     print ( "A problem occurred. Is the contents of the Images folder empty?" )
@@ -36,13 +40,30 @@ def myrandom ( mylist ): # This function returns a random element from a list su
     myrandom.previous = choice
     return choice
 
+def update_image():
+    FP = myrandom ( image_names ) # Get a random image 
+    img_path = os.path.join ( image_folder, FP )
+
+    img = Image.open ( img_path )
+    img = img.resize ( ( 400, 300 ) )
+    tk_img = ImageTk.PhotoImage ( img )
+
+    imlbl.config ( image = tk_img )
+    imlbl.image = tk_img
+ 
+    txtlbl.config ( text = os.path.splitext ( FP )[0] )
+
+    page.after ( 4000, update_image ) # 4 Secounds to show a new random image!
+
 page  = Tk ()
 page  . title ("Py Violin Sheet - Version 0.0.1")
 
-imlbl = Label(page)
+imlbl = Label ( page )
 imlbl . pack ()
 
-imlbl = Label ( page, font = ( "Arial", 30 ) )
-imlbl . pack ()
+txtlbl = Label ( page, font = ( "Arial", 30 ) )
+txtlbl . pack ()
+
+update_image ()
 
 page  . mainloop ()
