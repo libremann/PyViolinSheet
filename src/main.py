89f59ae0dc64db6e581fd4597456bb7a1775b522
Libreman <libreman@parabyte.com>
2025-07-28 19:33:35 +0330

    Improved error text for empty Images folder.

--- src/main.py
+++ src/main.py
@@ -22,6 +22,10 @@ import sys
 
 def errorfunc ():
     print ( "A problem occurred. Is the contents of the Images folder empty?" )
+    print ( "Is it not empty? you can send a message to my email or Telegram. The contact information is available on my GitHub profile page.")
+    print ("If you're experiencing an issue, please describe it on the following page so we can address it as quickly as possible:" )
+    print ( "\nGithub : https://github.com/libremann" )
+    print ("\nEnjoy :) ")
     sys.exit ( -1 )
 
 def myrandom ( mylist ): # This function returns a random element from a list such that no element is repeated consecutively.
